bd2d31b10a980be733daf987e481d5c0880f76c3
Jake Eaker <jacob.eaker@gmail.com>
2025-05-14 19:41:10 -0400

    Changed the way themes are handled in some respects, code blocks show much better in light mode now.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './theme.css'
+import '../public/global.css'
 
 console.log("🚀 Before importing App");
 import App from './App.jsx'
